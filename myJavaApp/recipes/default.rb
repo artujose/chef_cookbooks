4f23d3866085c6b56dc33ec9b1ac10bcec1e5f7b
Arturo Diaz <artu_jose@yahoo.com.mx>
2016-10-24 17:12:34 -0500

    Update default.rb

--- myJavaApp/recipes/default.rb
+++ myJavaApp/recipes/default.rb
@@ -16,8 +16,7 @@ template "/etc/init.d/myJavaApp_service" do
  source "myJavaApp_script.erb"
  mode 00700
  variables( 
-  server_port: node[:deploy]['myJavaApp'][:environment_variables][:server_port],
-  app_host: node[:deploy]['myJavaApp'][:environment_variables][:app_host] )
+  server_port: node[:deploy]['myJavaApp'][:environment_variables][:server_port], app_host: node[:deploy]['myJavaApp'][:environment_variables][:app_host] )
 end
 
 service "myJavaApp_service" do
