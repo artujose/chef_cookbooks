85c5339b0dc7b86e05f41a46cb12ec62c652ac6b
Arturo Diaz <artu_jose@yahoo.com.mx>
2016-10-27 22:51:57 -0500

    Update default.rb

--- myJavaApp/recipes/default.rb
+++ myJavaApp/recipes/default.rb
@@ -4,7 +4,7 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-temp=Chef::Config[:file_cache_path]
+directoryJavaApp=Chef::Config[:file_cache_path]
 
  remote_file "#{Chef::Config[:file_cache_path]}/myJavaApp.jar" do
      source "https://github.com/artujose/com.test.app/blob/master/target/com.test.app-1.0.0-SNAPSHOT.jar"
@@ -13,6 +13,6 @@ temp=Chef::Config[:file_cache_path]
 
  bash 'run_jar' do
      code <<-EOF
-       java -jar #{temp}/myJavaApp.jar > #{temp}/myJavaApp.log 2>&1 &
+       java -jar #{directoryJavaApp}/myJavaApp.jar > #{directoryJavaApp}/myJavaApp.log 2>&1 &
      EOF
  end
